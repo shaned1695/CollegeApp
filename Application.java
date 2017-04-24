a3d742e651e0c80242bda071e94e0cce9013c6b9
shaned1695 <shanedunleavy@live.ie>
2017-04-24 11:51:30 +0100

    Add files via upload

--- Application.java
+++ Application.java
@@ -3,8 +3,14 @@ public class Application {
 	private String course_name;
 	private int course_number
 
-    public Application() {
-    }
+    public Application(String course_name, int course_number) {
+    	this.course_name = ((course_name.length()>0)?course_name: "Unknown");
+    	this.course_number = ((course_number >0)?course_number: 1);
+    }//end constructor
 
 
-}
+	public String toString(){
+		return "\nCourse Name: " + course_name + "\nCourse Number: " + course_number;
+	}//end toString
+
+}//end class
