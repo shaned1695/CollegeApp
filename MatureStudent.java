2ddf172c3a9bb1b72800074532ec703cca9f501a
shaned1695 <shanedunleavy@live.ie>
2017-04-25 23:27:35 +0100

    Add files via upload

--- MatureStudent.java
+++ MatureStudent.java
@@ -4,9 +4,16 @@ public class MatureStudent {
 
 	//constructor
     public MatureStudent(String name, int id, String address, Application a1, String DoB) {
-    	super(name, id, address, a1);
+    	//super(name, id, address, a1);
     	this.DoB = DoB;
     }//end constructor
+
+    public void setDoB(String DoB){
+    	this.DoB = DoB;
+    }
+    public String getDoB(){
+    	return DoB;
+    }
 
     public String toString(){
     	return super.toString() + "\nDate of Birth: " + DoB;
