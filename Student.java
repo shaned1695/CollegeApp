00ec9d0601ac9a58048c3a47e9cb7d738b6cddf1
shaned1695 <shanedunleavy@live.ie>
2017-04-25 23:15:46 +0100

    Add files via upload

--- Student.java
+++ Student.java
@@ -3,8 +3,11 @@ public class Student {
 	private String address;
 	private Application a1;
 
-    public Student() {
-    }
+    public Student(String name, int id, String address, Application a1) {
+    	super (name,id);
+    	this.address = ((address.length()>0)?address: "Unknown");
+    	this.a1=a1;
+    }//end constuctor
 
 
 }
