94f5a089d592bc1b66588c053f15086fce97b2f4
shaned1695 <shanedunleavy@live.ie>
2017-04-26 11:34:19 +0100

    Add files via upload

--- StudentTest2.java
+++ StudentTest2.java
@@ -7,7 +7,17 @@ public class StudentTest2 {
 	static int sub = 0;
 
 
-    public StudentTest2() {
+    public static void main(String args[]) {
+
+    	//declare variables
+    	int ID,Course_Number,userID,choice = 0,numrecords,menu = 0,mchoice = 0,changeap = 0,NewCourseID,changest = 0,NewStudentID;
+    	String Name,Course_Name,Address,mstudent,Date_Of_Birth="",NewCourseName = "",NewStudentName = "",strID,strCourse_Number;
+    	String struserID,strchoice,strnumrecords,strmenu,strmchoice,strchangeap,strnewcourseID,strchangest,strnewstudentID;
+
+    	char check1,check2;
+    	boolean MatureFlag = false;
+    	boolean flag = false;
+
     }
 
 
