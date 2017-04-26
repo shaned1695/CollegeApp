d5b887afe6d5c0ce18c41a8a97b58b1d14714c66
shaned1695 <shanedunleavy@live.ie>
2017-04-26 11:32:49 +0100

    Add files via upload

---
+++ StudentTest2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Date;
+import java.util.Scanner;
+public class StudentTest2 {
+
+	static Scanner input = new Scanner(System.in);
+	static int sub = 0;
+
+
+    public StudentTest2() {
+    }
+
+
+}
